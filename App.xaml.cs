d10352ab1f32484b41e302d0d69413993e433ff7
Saafie <ST10446806@rcconnect.edu.za>
2025-09-09 16:29:19 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ST10446806_PROG6212_POEPART1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ LecturerWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ST10446806_PROG6212_POEPART1
+{
+    public partial class LecturerWindow : Window
+    {
+        public LecturerWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
